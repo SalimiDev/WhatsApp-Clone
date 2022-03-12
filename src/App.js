10c75865feafd314dbc61f7c987f12429a40b525
Mehdi Salimi <Salimi.Devop@gmail.com>
2022-03-12 23:36:01 +0330

    feat:added search bar UI and routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
+import React ,{useEffect} from 'react';
 import styles from './App.module.css';
 import { auth } from './firebase';
+import { Route, Routes, Navigate, useNavigate } from 'react-router-dom';
 //hooks
 import useWindowSize from './hooks/useWindowSize';
 import useAuthUser from './hooks/useAuthUser';
 //Components
 import Login from './components/Login';
 import Sidebar from './components/Sidebar';
-
-function App() {
+import Chat from './components/Chat';
+const App=()=> {
       const user = useAuthUser(auth);
       const page = useWindowSize();
+      const navigate = useNavigate();
+      useEffect(() => {
+            if (page.isMobile) {
+                  navigate('/chats');
+            } else {
+                  navigate('/');
+            }
+      }, [page.isMobile]);
 
       if (!user) {
             return <Login />;
       }
+
       return (
             <div className={styles.App} style={{ ...page }}>
                   <div className={styles.app__body}>
                         <Sidebar user={user} page={page} />
+                        <Routes>
+                              <Route path='/room/:roomId' element={<Chat user={user} page={page} />} />
+                        </Routes>
                   </div>
             </div>
       );
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,28 +1,35 @@
 import React from 'react';
 import styles from './Sidebar.module.css';
 import { Avatar, IconButton } from '@material-ui/core';
+import { SearchOutlined } from '@material-ui/icons';
 import { ExitToApp } from '@material-ui/icons';
 import { auth } from '../firebase';
 
 const Sidebar = ({ user, page }) => {
-      const SignOutOnClick = () => {
-            auth.signOut();
-      };
-      return (
-            <div className={styles.sidebar} style={{ minHeight: page.isMobile ? page.height : 'auto' }}>
-                  <div className={styles.sidebar__header}>
-                        <div className={styles.sidebar__headerLeft}>
-                              <Avatar src={user?.photoURL} />
-                              <h4>{user?.displayName}</h4>
-                        </div>
-                        <div className={styles.sidebar__headerRight}>
-                              <IconButton onClick={SignOutOnClick}>
-                                    <ExitToApp />
-                              </IconButton>
-                        </div>
-                  </div>
+    const SignOutOnClick = () => {
+        auth.signOut();
+    };
+    return (
+        <div className={styles.sidebar} style={{ minHeight: page.isMobile ? page.height : 'auto' }}>
+            <div className={styles.sidebar__header}>
+                <div className={styles.sidebar__headerLeft}>
+                    <Avatar src={user?.photoURL} />
+                    <h4>{user?.displayName}</h4>
+                </div>
+                <div className={styles.sidebar__headerRight}>
+                    <IconButton onClick={SignOutOnClick}>
+                        <ExitToApp />
+                    </IconButton>
+                </div>
             </div>
-      );
+            <div className={styles.sidebar__search}>
+                <form className={styles.sidebar__searchContainer}>
+                    <SearchOutlined />
+                    <input type='text' id='search' placeholder='Search for users or rooms' />
+                </form>
+            </div>
+        </div>
+    );
 };
 
 export default Sidebar;
