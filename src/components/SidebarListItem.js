52c1858626bc0bdc4fc3eb77f687b61104156e1c
Mehdi Salimi <Salimi.Devop@gmail.com>
2022-03-14 01:56:26 +0330

    style:added Sidebar listItem's styles

--- src/components/SidebarListItem.js
+++ src/components/SidebarListItem.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import styles from './SidebarListItem.module.css';
 import { Avatar } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 
 const SidebarListItem = ({ item }) => {
     return (
-        <Link to={`/room/${item.id}`}>
-            <div>
-                <div>
+        <Link className={styles.link} to={`/room/${item.id}`}>
+            <div className={styles.sidebar__chat}>
+                <div className={styles.avatar__container}>
                     <Avatar
                         style={{
                             width: 45,
@@ -15,7 +16,7 @@ const SidebarListItem = ({ item }) => {
                         src={item.photoURL || `https://avatars.dicebear.com/api/human/${item.id}.svg`}
                     />
                 </div>
-                <div>
+                <div className={styles.sidebar__chatInfo}>
                     <h2>{item.name}</h2>
                 </div>
             </div>
