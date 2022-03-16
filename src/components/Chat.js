ccbb0b66253a824f96cd689b0ec8a0f5d1d2078a
Mehdi Salimi <Salimi.Devop@gmail.com>
2022-03-16 19:09:17 +0330

    feat:created useRoom custom hook

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+//hooks
+import useRoom from '../hooks/useRoom';
 
-const Chat = () => {
-    return (
-        <div>
-            chats
-        </div>
-    );
+const Chat = ({ user, page }) => {
+    //hooks & Routes
+    const { roomId } = useParams();
+    const room = useRoom(roomId, user.uid);
+
+    return <div>chats</div>;
 };
 
 export default Chat;
---
+++ src/hooks/useRoom.js
@@ -0,0 +1,18 @@
+import { useDocument } from 'react-firebase-hooks/firestore';
+import { db } from '../firebase';
+const useRoom = (roomId, userId) => {
+    const isUserRoom = roomId.includes(userId);
+
+    const doc = isUserRoom ? roomId?.replace(userId, '') : roomId;
+
+    const [snapshot] = useDocument(db.collection(isUserRoom ? 'users' : 'rooms').doc(doc));
+
+    if (!snapshot) return null;
+
+    return {
+        id: snapshot.id,
+        photoURL: snapshot.photoURL || `https://avatars.dicebear.com/api/human/${snapshot.id}.svg`,
+        ...snapshot.data(),
+    };
+};
+export default useRoom;
