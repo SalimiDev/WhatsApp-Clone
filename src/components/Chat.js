4d81b8f379d558a1e1b4939392be4eee75a962f5
Mehdi Salimi <Salimi.Devop@gmail.com>
2022-03-16 20:26:04 +0330

    feat:added header of chat section

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,5 +1,9 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
+//Styles
+import styles from './Chat.module.css';
+import { Avatar, IconButton } from '@material-ui/core';
+import { AddPhotoAlternate, ArrowBack, MoreVert } from '@material-ui/icons';
 //hooks
 import useRoom from '../hooks/useRoom';
 
@@ -7,8 +11,41 @@ const Chat = ({ user, page }) => {
     //hooks & Routes
     const { roomId } = useParams();
     const room = useRoom(roomId, user.uid);
+    const navigate = useNavigate();
 
-    return <div>chats</div>;
+    return (
+        <div className={styles.chat}>
+            <div style={{ height: page.height }} className={styles.chat__background} />
+
+            <div className={styles.chat__header}>
+                {page.isMobile && (
+                    <IconButton onClick={navigate(-1)}>
+                        <ArrowBack />
+                    </IconButton>
+                )}
+
+                <div className={styles.avatar__container}>
+                    <Avatar src={room?.photoURL} />
+                </div>
+
+                <div className={styles.chat__headerInfo}>
+                    <h3 style={{ width: page.isMobile && page.width - 165 }}>{room?.name}</h3>
+                </div>
+
+                <div className={styles.chat__headerRight}>
+                    <input id='image' style={{ display: 'none' }} accept='image/*' type='file' />
+                    <IconButton>
+                        <label style={{ cursor: 'pointer', height: 24 }} htmlFor='image'>
+                            <AddPhotoAlternate />
+                        </label>
+                    </IconButton>
+                    <IconButton>
+                        <MoreVert />
+                    </IconButton>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Chat;
